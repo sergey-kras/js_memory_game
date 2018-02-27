4a642e78a192143491f1d67bdf4fbf5d37b8a1b1
sergey-kras <vip.41243@gmail.com>
2018-02-28 02:41:04 +0700

    Added OOP

--- app/js/common.js
+++ app/js/common.js
@@ -1,58 +1,59 @@
-var deck = [
-    'KC', 'KD', 'KH', 'KS' ,
-    '0C', '0D', '0H', '0S' ,
-    '2C', '2D', '2H', '2S' ,
-    '3C', '3D', '3H', '3S' ,
-    '4C', '4D', '4H', '4S' ,
-    '5C', '5D', '5H', '5S' ,
-    '6C', '6D', '6H', '6S' ,
-    '7C', '7D', '7H', '7S' ,
-    '8C', '8D', '8H', '8S' ,
-    '9C', '9D', '9H', '9S' ,
-    'AC', 'AD', 'AH', 'AS' ,
-    'JC', 'JD', 'JH', 'JS' ,
-    'QC', 'QD', 'QH', 'QS'
-];
-function randomCards(deck) {
-    var Cards = [];
-    for (var i=0; i<9; i++){
-        var nowElement = CheckMatch(deck,Cards);
-        Cards.push(nowElement);
-        Cards.push(nowElement);
+var Deck = new Object();
+Cards = {
+    deck : [
+        'KC', 'KD', 'KH', 'KS' ,
+        '0C', '0D', '0H', '0S' ,
+        '2C', '2D', '2H', '2S' ,
+        '3C', '3D', '3H', '3S' ,
+        '4C', '4D', '4H', '4S' ,
+        '5C', '5D', '5H', '5S' ,
+        '6C', '6D', '6H', '6S' ,
+        '7C', '7D', '7H', '7S' ,
+        '8C', '8D', '8H', '8S' ,
+        '9C', '9D', '9H', '9S' ,
+        'AC', 'AD', 'AH', 'AS' ,
+        'JC', 'JD', 'JH', 'JS' ,
+        'QC', 'QD', 'QH', 'QS'
+    ],
+    randomCards: function (deck) {
+        var Cards = [];
+        for (var i=0; i<9; i++){
+            var nowElement = this.CheckMatch(deck,Cards);
+            Cards.push(nowElement);
+            Cards.push(nowElement);
+        }
+        return this.randomAll(Cards);
+    },
+    CheckMatch: function (deck, Cards) {
+        var nowElement = deck[Math.floor(Math.random()*deck.length)];
+        if(Cards.indexOf(nowElement) == -1){
+            return nowElement;
+        }
+        else {
+            return this.CheckMatch(deck, Cards);
+        }
+    },
+    random: function (min, max) {
+        var range = max - min + 1;
+        return Math.floor(Math.random()*range) + min;
+    },
+    randomAll: function (randomDeck) {
+        var r_i;
+        var v;
+        for (var i = 0; i < randomDeck.length-1; i++) {
+            r_i = this.random(0, randomDeck.length-1);
+            v = randomDeck[r_i];
+            randomDeck[r_i] = randomDeck[randomDeck.length-1];
+            randomDeck[randomDeck.length-1] = v;
+        }
+        return randomDeck;
+    },
+    viewCards: function (deck) {
+        for(var i = 0; i < 18; i++){
+            $('.game-page_field').append('<div class="card"><img src="img/cards/'+deck[i]+'.png" alt=""></div>');
+        }
     }
-    // Cards
-    return randomAll(Cards);
-}
-function CheckMatch(deck, Cards) {
-    var nowElement = deck[Math.floor(Math.random()*deck.length)];
-    if(Cards.indexOf(nowElement) == -1){
-        return nowElement;
-    }
-    else {
-        return CheckMatch(deck, Cards);
-    }
-}
-function random(min, max) {
-    var range = max - min + 1;
-    return Math.floor(Math.random()*range) + min;
-}
-function randomAll(randomDeck) {
-    var r_i;
-    var v;
-    for (var i = 0; i < randomDeck.length-1; i++) {
-        r_i = random(0, randomDeck.length-1);
-        v = randomDeck[r_i];
-        randomDeck[r_i] = randomDeck[randomDeck.length-1];
-        randomDeck[randomDeck.length-1] = v;
-    }
-    return randomDeck;
-}
-var Cards = randomCards(deck);
-
-function viewCards(deck) {
-    for(var i = 0; i < 18; i++){
-        $('.game-page_field').append('<div class="card"><img src="img/cards/'+Cards[i]+'.png" alt=""></div>');
-    }
-}
-viewCards(deck);
-
+};
+var Deck = Cards.randomCards(Cards.deck);
+Deck = Cards.randomAll(Deck);
+Cards.viewCards(Deck);
